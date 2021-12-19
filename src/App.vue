c507f1dc4d78779adf98aa9424fb7d0a4c1a615b
odin <odin@gmail.com>
2021-12-20 01:26:36 +0800

    引入 naive-ui

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <p>组件</p>
+  <n-button @click="handleClick">点击</n-button>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script setup lang="ts">
+
+import { onMounted } from 'vue'
+import { NButton } from 'naive-ui'
+
+onMounted(() => {
+  console.log('mounted')
+})
+
+const handleClick = () => {
+  console.log('click')
 }
+
+</script>
+
+<style>
 </style>
