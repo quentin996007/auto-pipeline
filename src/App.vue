f48bf49f4ac2f7ed7d8205a43fceeeb1a49e3f5f
odin <odin@gmail.com>
2021-12-20 02:41:22 +0800

    配置 tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
   <p>组件</p>
-  <n-button @click="handleClick">点击</n-button>
+  <n-config-provider :theme="theme">
+    <n-card>
+      <n-space>
+        <n-button @click="theme = darkTheme">深色</n-button>
+        <n-button @click="theme = null">浅色</n-button>
+      </n-space>
+    </n-card>
+  </n-config-provider>
+
+  <h1 class="text-3xl font-bold underline text-red-600">Hello world!</h1>
 </template>
 
 <script setup lang="ts">
 
-import { onMounted } from 'vue'
-import { NButton } from 'naive-ui'
+import { onMounted, Ref, ref } from 'vue'
+import { NButton, darkTheme } from 'naive-ui'
+import { BuiltInGlobalTheme } from 'naive-ui/lib/themes/interface'
+
+const theme: Ref<BuiltInGlobalTheme | null> = ref(null)
 
 onMounted(() => {
   console.log('mounted')
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import naive from 'naive-ui'
+import './index.css'
 
-createApp(App).mount('#app')
+// 通用字体
+// import 'vfonts/Lato.css'
+// 等宽字体
+// import 'vfonts/FiraCode.css'
+
+const app = createApp(App)
+
+app.use(naive)
+
+app.mount('#app')
